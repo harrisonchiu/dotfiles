0aa165bd19c542c3f0a984e4b739f64abcbee899
harrisonchiu <32177072+harrisonchiu@users.noreply.github.com>
2023-04-17 19:25:27 -0400

    Added Firefox userChrome.css and JS scriptlets for ublock origin

---
+++ ublockScriplets.js
@@ -0,0 +1,36 @@
+/// youtube-shorts-redirector.js
+/// alias ysr.js
+(function () {
+    "use strict";
+    let oldHref = document.location.href;
+    if (window.location.href.indexOf("youtube.com/shorts") > -1) {
+        window.location.replace(window.location.toString().replace("/shorts/", "/watch?v="));
+    }
+    window.onload = function () {
+        let bodyList = document.querySelector("body");
+        let observer = new MutationObserver(function (mutations) {
+            mutations.forEach(function () {
+                if (oldHref !== document.location.href) {
+                    oldHref = document.location.href;
+                    if (window.location.href.indexOf("youtube.com/shorts") > -1) {
+                        window.location.replace(window.location.toString().replace("/shorts/", "/watch?v="));
+                    }
+                }
+            });
+        });
+        let config = {
+            childList: true,
+            subtree: true
+        };
+        observer.observe(bodyList, config);
+    };
+})();
+
+/// old-reddit-redirector.js
+/// alias orr.js
+(function () {
+    "use strict";
+    if (window.location.href.includes("/www.reddit.com/") && !window.location.href.includes("/www.reddit.com/gallery/") && !window.location.href.includes("/www.reddit.com/poll/")) {
+        window.location.replace(window.location.toString().replace("/www.reddit.com/", "/old.reddit.com/"));
+    }
+})();
